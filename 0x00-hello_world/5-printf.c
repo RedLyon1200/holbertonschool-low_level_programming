3599ab006f6db842c38e13fc456cab9c1ee27620
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-05 19:05:59 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print message with PRINTF command
+ * Return: void = 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
