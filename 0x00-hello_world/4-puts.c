079c79aa0e7549e3b3817459f45225f77cc24f87
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-05 18:24:23 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  *
  *Description: uses puts to output fixed message :)
  *
  */
-int main (void) {
-  puts("\"Programming is like building a multilingual puzzle\n");
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
