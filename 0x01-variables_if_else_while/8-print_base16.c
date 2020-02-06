f7221884cf9cff7ae60cb064d7e377bde3146425
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-06 19:13:45 +0000

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (seccess)
+ */
+int main(void){
+char a, b;
+a = 48; /* 48 is 0 on ASCII */
+b = 97; /* 97 is a on ASCII */
+while (a <= 57) /* 57 is 9 on ASCII */
+{
+putchar(a);
+a++;
+}
+while (b <=102) /* 102 is f on ASCCI */
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return (0);
+}
