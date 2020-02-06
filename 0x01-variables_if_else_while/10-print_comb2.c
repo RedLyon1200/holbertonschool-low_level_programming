7df6c24dc1cbcd67b5e828fb17b0a70a122ba080
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-06 19:57:54 +0000

    Task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int a, b;
+a = 0;
+while (a <= 9)
+{
+b = 00;
+while (b <= 9)
+{
+putchar('0' + a);
+putchar('0' + b);
+if (a != 9 || b != 9)
+{
+putchar(',');
+putchar(' ');
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+return (0);
+}
