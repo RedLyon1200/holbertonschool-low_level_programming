3786be06e7f76731fba4ea28ae86895db235139d
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-11 13:50:22 -0500

    Task 14 fished

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-/**
- *
- */
+
 int main(void)
 {
-	int num1 = 1, num2 = 2, num3, fib, count = 0;
+	long num1 = 1, num2 = 2, num3, fib, count = 0;
 
-	printf("%d,", num1);
-	printf("%d,", num2);
-	for (count = 0; count <= 96; count++)
+	printf("%ld, ", num1);
+	printf("%ld, ", num2);
+	for (count = 0; count <= 47; count++)
 	{
 		fib = num1 + num2;
 		num3 = fib;
 		num1 = num2;
 		num2 = num3;
-		if (count == 96)
-		       	printf("%d", fib);
+		if (count == 47)
+			printf("%ld", fib);
 		else
-			printf("%d,", fib);
+			printf("%ld, ", fib);
 	}
 	printf("\n");
 
