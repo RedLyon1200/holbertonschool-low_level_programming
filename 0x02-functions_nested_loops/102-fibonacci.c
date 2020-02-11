5ebc3788c7700bdcebfeffdec198c4e199d6593d
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-11 13:31:33 -0500

    Task 14 fished - 100 items

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,13 @@ int main(void)
 
 	printf("%d,", num1);
 	printf("%d,", num2);
-	for (count = 0; count <= 46; count++)
+	for (count = 0; count <= 96; count++)
 	{
 		fib = num1 + num2;
 		num3 = fib;
 		num1 = num2;
 		num2 = num3;
-		if (count == 46)
+		if (count == 96)
 		       	printf("%d", fib);
 		else
 			printf("%d,", fib);
