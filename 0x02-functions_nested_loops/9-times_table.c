ccde02c6010c67ccdb92261db3f1934af45ab7f6
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-10 22:09:40 -0500

    Task 9 and its holberton.h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * times_table - asdasda
+ */
+void times_table(void)
+{
+	int a, b;
+	int comma = 44, space = 32;
+
+	for (a = 0; a <= 9; a++)
+	{
+		b = 0;
+		_putchar('0');
+		for (b = 1; b <= 9; b++)
+		{
+			if((a * b) <= 9)
+			{
+				_putchar(comma);
+				_putchar(space);
+				_putchar(space);
+				_putchar(a * b + '0');
+			}
+			else
+			{
+				_putchar(comma);
+				_putchar(space);
+				_putchar(a * b / 10 + '0');
+				_putchar(a * b % 10 + '0');
+			}
+		}
+		_putchar(10);
+	}
+}
