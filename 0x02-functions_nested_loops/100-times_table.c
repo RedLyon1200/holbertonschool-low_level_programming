0a45cfc607d3a7444b138d48d4584747e0c9dd74
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-10 23:14:37 -0500

    Task 12 and its holberton.h

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+void print_times_table(int n)
+{
+	int a, b, c;
+	int comma = 44, space = 32;
+
+	if (n > 0 && n < 15)
+	{
+		for ( a = 0 ; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b <= n; b++)
+			{
+				c = a * b;
+				if (c <= 9)
+				{
+					_putchar(comma);
+					_putchar(space);
+					_putchar(space);
+					_putchar(space);
+					_putchar(c + '0');
+				}
+				else if (c > 9 && c <= 99)
+				{
+					_putchar(comma);
+					_putchar(space);
+					_putchar(space);
+					_putchar(c / 10 + '0');
+					_putchar(c % 10 + '0');
+				}
+				else
+				{
+					_putchar(comma);
+					_putchar(space);
+					_putchar(c / 100 + '0');
+					_putchar(c / 10 % 10 + '0');
+					_putchar(c % 10 + '0');
+				}
+			}
+			_putchar(10);
+		}
+	}
+}
