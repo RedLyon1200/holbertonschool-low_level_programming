d875c04bdd3df44296185f396531c1d86f1b4226
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-18 23:29:26 -0500

    Tasks [7..11] and its holberton.h

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - generates a password for 101-crackme
+ *
+ * description: 101-crackme adds up the values of each ASCII character
+ * in the argument given, and they must equal 2772 to be a valid password.
+ * keygen generates random values between 32 and 127 and adds them up until it
+ * reaches 2772
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char password[100];
+	int randNum, num, i;
+
+	num = 0;
+	i = 0;
+	srand(time(NULL));
+	while (num < 2645)
+	{
+		randNum = random() % 127;
+		if (randNum > 32)
+		{
+			password[i++] = randNum;
+			num += randNum;
+		}
+	}
+	password[i++] = (2772 - num);
+	password[i] = '\0';
+	printf("%s", password);
+
+	return (0);
+}
