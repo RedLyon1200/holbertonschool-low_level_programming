47704b1a9010d4d06ee354cd99d52a18673e019f
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-24 21:01:10 +0000

    Task 5 and 6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0, k = i; needle[j] != '\0'; j++, k++)
+		{
+			if (needle[j] != haystack[k] || haystack[k] == '\0')
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return (0);
+}
