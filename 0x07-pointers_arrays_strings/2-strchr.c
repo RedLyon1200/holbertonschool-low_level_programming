be2b322a4f15812da6967a61c92c7af8e92ded77
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-24 19:08:50 +0000

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strchr - description
+ * @s: string
+ * @c: character
+ * Return: result
+ */
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	for (count = 0; s[count] != c; count++)
+	{
+		if (s[count] == c)
+			return (s + count);
+	}
+	if (s[count] == c)
+		return (s + count);
+
+	return (00);
+}
