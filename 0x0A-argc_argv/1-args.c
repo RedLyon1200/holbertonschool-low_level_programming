fdca489cb7dd3624f583463a88df03f7d4204ee6
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-28 19:18:43 +0000

    Task 1 | Test 0 || Task 0 | Betty fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - print file name
+ * main - print number of arguments passed
  * @argc: number of arguments
  * @argv: arguments
  *
- * Return: success
+ * Return: 0 success
  */
 int main(int argc, char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
