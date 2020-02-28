5d189358cbc45f5cfb9f82dda00da5437919892b
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-28 19:25:29 +0000

    Task 2 | Test 0

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all arguments passed
+ * @argc: number of arguments passed
+ * @argv: arguments passed
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
