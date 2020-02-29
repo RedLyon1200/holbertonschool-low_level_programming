32cf75b19ff86a726eab36512b0b37b78668d820
RedLyon1200 <sagudelo1200@gmail.com>
2020-02-29 12:31:01 -0500

    Task 5 | Fix 1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+/**
+ * change - change cents
+ * @n: cents total value
+ *
+ * Return: change cents
+ */
+int change(int n)
+{
+	int cents = 0;
 
+	if (n >= 0)
+	{
+		while (n - 25 >= 0)
+		{
+			cents++;
+			n = n - 25;
+		}
+		while (n - 10 >= 0)
+		{
+			cents++;
+			n = n - 10;
+		}
+		while (n - 5 >= 0)
+		{
+			cents++;
+			n = n - 5;
+		}
+		while (n - 2 >= 0)
+		{
+			cents++;
+			n = n - 2;
+		}
+		while (n - 1 >= 0)
+		{
+			cents++;
+			n = n - 1;
+		}
+		return (cents);
+	}
+	return (cents);
+}
+
+
+/**
+ * main - call change cents function
+ * @argc: number of arguments
+ * @argv: arguments passed
+ *
+ * Return: 0 success or 1 Error
+ */
 int main(int argc, char *argv[])
 {
 	int n, cents;
@@ -9,46 +58,20 @@ int main(int argc, char *argv[])
 	if (argc > 1)
 		n = atoi(argv[1]);
 
-	if (argc == 2)
+	if (n >= 0)
 	{
-		if (n >= 0)
+		if (argc == 2)
 		{
-			while (n - 25 >= 0)
-			{
-				cents++;
-				n = n - 25;
-			}
-			while (n - 10 >= 0)
-			{
-				cents++;
-				n = n - 10;
-			}
-			while (n - 5 >= 0)
-			{
-				cents++;
-				n = n - 5;
-			}
-			while (n - 2 >= 0)
-			{
-				cents++;
-				n = n - 2;
-			}
-			while (n - 1 >= 0)
-			{
-				cents++;
-				n = n - 1;
-			}
+			cents = change(n);
 			printf("%d\n", cents);
 		}
 		else
 		{
-			printf("0\n");
+			printf("Error\n");
+			return (1);
 		}
 	}
 	else
-	{
-		printf("Error\n");
-		return (1);
-	}
+		printf("0\n");
 	return (0);
 }
