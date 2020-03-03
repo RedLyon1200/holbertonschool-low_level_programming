eca9e050f7668b163b63fc6d850a3bf149676cb4
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-03 19:37:08 +0000

    Task 2 | Fix 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0;
 	char *conc;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
