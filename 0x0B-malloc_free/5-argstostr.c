21b83e979c97db029e7f2bf9f1e4ad6d7263b479
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-03 21:26:23 -0500

    Task 5 | Test 1

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -5,37 +5,34 @@
  * @ac: number of arguments
  * @av: double pointer to array passed
  *
- * Return: NULL if fail, else return pointer to new stringr
+ * Return: pointer to new buffer with all arguments as 1 string.
  */
-
 char *argstostr(int ac, char **av)
 {
-	int i = 0;
-	int j;
-	int x;
-	int y;
+	int i, j, len, bufferlen;
 	char *p;
 
-	if (ac == 0 || av == NULL)
-	{
+	if (ac == 0)
 		return (NULL);
-	}
-	for (i = 0, x = 0, y = 0; x < ac; i++)
-	{
-		if (av[x][y] == '\0')
-		{
-			x++;
-		}
-	}
-	p = malloc(i * sizeof(char));
-	if (p == NULL)
-	{
+	if (av == NULL)
 		return (NULL);
-	}
-	for (i = 0, j = 0; i < ac; i++, j++)
-	{
-		p[j] = av[i][j];
-	}
 
+	i = j = len = bufferlen = 0;
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+	}
+	p = (char *)malloc(len * sizeof(char) + ac + 1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++, bufferlen++)
+			p[bufferlen] = av[i][j];
+		p[bufferlen] = '\n';
+		bufferlen++;
+	}
+	p[bufferlen] = '\0';
 	return (p);
 }
