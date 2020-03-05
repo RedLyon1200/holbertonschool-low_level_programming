5d5bf39eba2c78a996fcbe65651dae5e3040d149
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-05 20:08:50 +0000

    Task 1 | Test 4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,9 +1,7 @@
-#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * string_nconcat - concatenate 2 strings
- * @s1: string 1
+ * string_nconcat - concatenate 2 strings* @s1: string 1
  * @s2: string 2
  * @n: bytes to concatenate
  * Return: pointer to concatenated string
@@ -15,10 +13,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
@@ -38,9 +34,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		conc[i] = s1[i];
 
-	for (j = 0; j < n; j++)
-		conc[i + j] = s2[j];
-	if (j == n)
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+                conc[i + j] = s2[j];
+	if (j == n || s2[j] == '\0')
 		conc[i + j] = '\0';
 	return (conc);
 }
