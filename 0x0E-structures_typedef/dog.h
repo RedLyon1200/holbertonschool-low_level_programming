b9ee8cd6e808551ef02f0b0a2d9e060d90668098
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-09 16:06:50 -0500

    Task 0 | Test 0 </> Added README.md file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef CRASHED_MACHINE
+#define CRASHED_MACHINE
+
+/**
+ * struct dog - dogs database v:
+ * @name: dog name
+ * @owner: owner name
+ * @age: dog age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
