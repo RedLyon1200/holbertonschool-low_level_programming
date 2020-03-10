5e61d7501cd942d5f366bfa5705e88f9cdaecc5e
CRASHED_MACHINE <sagudelo1200@gmail.com>
2020-03-09 20:37:28 -0500

    Task 4 | Test 0

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,35 +1,75 @@
 #include "dog.h"
+#include <stdlib.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: character
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (*(s + i))
+	{
+		i++;
+	}
+	return (i);
+}
 
 /**
- * _strncpy - copy a string
- * @dest: character dest
- * @src: constant character
- * @n: variable
- * Return: dest
+ * *_strcpy - copies string pointed to by src
+ * @dest: character pointer
+ * @src: character source
+ *Return: the pointer to dest
  */
-
-char *_strncpy(char *dest, char *src, int n)
+char *_strcpy(char *dest, char *src)
 {
-	int size = 0;
+	int i;
 
-	while (size < n && src[size] != '\0')
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		dest[size] = src[size];
-		size++;
+		*(dest + i) = *(src + i);
 	}
-	for (; size < n; size++)
-		dest[size] = '\0';
 
 	return (dest);
 }
 
-
-
-
 /**
+ * new_dog - create a new dog
+ * @name: char string name
+ * @age: int age
+ * @owner: char string owner
+ * Return: pointer to new dog
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	
+	struct dog *dog_new;
+	int namelen;
+	int ownerlen;
+
+	dog_new = malloc(sizeof(struct dog));
+	if (dog_new == NULL)
+		return (NULL);
+
+	namelen = _strlen(name);
+	dog_new->name = malloc(sizeof(char) * (namelen + 1));
+	if (dog_new->name == NULL)
+	{
+		free(dog_new);
+		return (NULL);
+	}
+	dog_new->name = _strcpy(dog_new->name, name);
+
+	ownerlen = _strlen(owner);
+	dog_new->owner = malloc(sizeof(char) * (ownerlen + 1));
+	if (dog_new->owner == NULL)
+	{
+		free(dog_new->name);
+		free(dog_new);
+		return (NULL);
+	}
+	dog_new->owner = _strcpy(dog_new->owner, owner);
+	dog_new->age = age;
+
+	return (dog_new);
 }
