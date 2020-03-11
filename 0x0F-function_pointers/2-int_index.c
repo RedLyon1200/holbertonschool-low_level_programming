08a44f3012452920eccb07adf016bb118fefe863
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-11 14:40:20 -0500

    Task 2 | conditional if size <= 0 was fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 	int result = 0;
 
-	if (size < ((1 + 45 - 15 + 10) * 0) + 1)
+	if (size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
