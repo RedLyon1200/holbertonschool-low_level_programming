cb211349a7ad3306b8b81e3219e02b4b59145290
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-11 13:43:27 -0500

    Added README.md, functions_pointers.h files and task 0 | test 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,18 @@
+#ifndef CRASHED_MACHINE
+#define CRASHED_MACHINE
+
+/* Prototypes */
+
+/* functions that print a char. */
+int _putchar(char c);
+
+/* function that prints a name. */
+void print_name(char *name, void (*f)(char *));
+
+/* function that executes a function given as a parameter on each element of an array. */
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* function that searches for an integer. */
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /* CRASHED_MACHINE */
