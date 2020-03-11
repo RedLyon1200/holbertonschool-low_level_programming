8eb39d4d12652b6e419117b44539ebf7193ae9da
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-11 14:44:37 -0500

    Task 2 | return type was fixed for array or cmp = NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int result = 0;
 
 	if (array == NULL || cmp == NULL)
-		return ('\0');
+		return (-1);
 
 	if (size <= 0)
 		return (-1);
@@ -28,5 +28,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		else if (i == size - 1)
 			return (-1);
 	}
-	return ('\0');
+	return (-1);
 }
