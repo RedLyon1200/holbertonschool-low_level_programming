85eea4d350339131b252302dcf8742e5a3591869
CRASHED_MACHINE <sagudelo1200@gmail.com>
2020-03-14 13:16:23 -0500

    .

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef CRASHEDMACHINE
+#define CRASHEDMACHINE
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void _printchar(va_list list);
+void _printstring(va_list list);
+void _printfloat(va_list list);
+void _printint(va_list list);
+#endif /* CRASHEDMACHINE */
