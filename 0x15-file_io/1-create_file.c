956d22b76f39e1b1c48d4c517c2d0e52bd57818b
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-31 13:38:17 -0500

    Task 1 was added for test 0, and it\'s holberton.h wass updated

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: is the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ *
+ * Return: -1 if error, 1 success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int err = 0, size = 0;
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	char *buffer;
+
+	if (text_content == NULL)
+		text_content = "";
+	if (fd < 0)
+	{
+		return (-1);
+	}
+	while (!text_content) /* size of content */
+		size++;
+
+	buffer = malloc(sizeof(*buffer) * size);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (-1);
+	}
+
+	err = write(fd, &buffer, size);
+	if (err < 0)
+	{
+		free(buffer);
+		return (-1);
+	}
+	close(fd);
+	free(buffer);
+	return (1);
+}
