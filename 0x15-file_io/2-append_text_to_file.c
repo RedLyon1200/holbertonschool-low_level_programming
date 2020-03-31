7913fe0fb35e46457059b02f9604abbb62b603bd
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-31 15:04:01 -0500

    Betty was fixed for the task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the name of the file
+ * @text_content: is the NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int data, size = 0;
@@ -7,6 +14,9 @@ int append_text_to_file(const char *filename, char *text_content)
 /* Error Validators */
 	if (filename == NULL)
 		return (-1);
+
+	if (text_content == NULL)
+		return (1);
 /* read */
 	data = open(filename, O_APPEND | O_WRONLY);
 
