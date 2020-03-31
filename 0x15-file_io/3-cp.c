a0529e8ab35c5873da12399035a6943d67a59570
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-31 17:47:43 -0500

    Task 3 and it's holberton.h was added for the test 0

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,89 @@
+#include "holberton.h"
+#define BUFFSIZE 1024
+
+/**
+ * _checker - check if files are NULL
+ * @from: file1
+ * @to: file2
+ */
+void _checker(char *from, char *to)
+{
+	if (from == NULL)
+		R_err(from);
+	if (to == NULL)
+		W_err(to);
+}
+
+/**
+ * C_err - print "close error" message
+ * @file: readed file
+ */
+void C_err(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file);
+	exit(100);
+}
+
+/**
+ * R_err - print "read error" message
+ * @file: readed file
+ */
+void R_err(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	exit(98);
+}
+
+/**
+ * W_err - print "write error" message
+ * @file: writed file
+ */
+void W_err(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(98);
+}
+
+
+/**
+ * main - that copies the content of a file to another file.
+ * @ac: arguments count
+ * @av: arguments vector
+ *
+ * Return: 0 success
+ */
+int main(int ac, char *av[])
+{
+	int file_from, file_to, data, retval;
+	char buff[BUFFSIZE];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	_checker(av[1], av[2]);
+	/* open files */
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	data = read(file_from, buff, BUFFSIZE);
+	if (data == -1)
+		R_err(av[1]);
+
+	while (data != 0)
+	{
+		retval = write(file_to, buff, data);
+		if (retval == -1 || retval != data)
+			W_err(av[2]);
+		data = read(file_from, buff, BUFFSIZE);
+		if (data == -1)
+			R_err(av[1]);
+	}
+	retval = close(file_from);
+	C_err(av[1]);
+	retval = close(file_to);
+	C_err(av[2]);
+	return (0);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -10,4 +10,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void W_err(char *file);
+void R_err(char *file);
+void C_err(char *file);
+void _checker(char *from, char *to);
+
 #endif
