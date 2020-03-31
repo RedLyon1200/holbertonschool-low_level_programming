7dd5d56360ee0265d931570b79833001fb0c8950
RedLyon1200 <sagudelo1200@gmail.com>
2020-03-31 14:07:21 -0500

    file permissions was fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int err = 0, size = 0;
 	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	char *buffer;
 
-	if (text_content == NULL)
-		text_content = "";
 	if (fd < 0)
 	{
 		return (-1);
@@ -22,20 +19,12 @@ int create_file(const char *filename, char *text_content)
 	while (!text_content[size]) /* size of content */
 		size++;
 
-	buffer = malloc(sizeof(*buffer) * size);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (-1);
-	}
 
 	err = write(fd, text_content, size);
 	if (err < 0)
 	{
-		free(buffer);
 		return (-1);
 	}
 	close(fd);
-	free(buffer);
 	return (1);
 }
