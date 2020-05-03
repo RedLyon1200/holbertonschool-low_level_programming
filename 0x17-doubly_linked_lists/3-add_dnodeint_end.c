4bbee233c6c253c2035edcf9dd133abb5e4ed6cc
Santiago Agudelo <sagudelo1200@hmail.com>
2020-05-03 00:46:42 -0500

    Task 7 | name of func add_node was changed to add_node2 for conflict with Task 2 for compile the Task 7

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+dlistint_t *new_node2(const int n);
 /**
  * add_dnodeint_end -  adds a new node at the end of a dlistint_t list.
  * @head: head of list
@@ -10,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *nNew = NULL, *nAux = *head;
 
-	nNew = new_node(n);
+	nNew = new_node2(n);
 	if (nNew != NULL)
 	{
 		if (!nAux)
@@ -33,12 +34,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 }
 
 /**
- * new_node - create a node
+ * new_node2 - create a node
  * @n: data
  *
  * Return: new node
  */
-dlistint_t *new_node(const int n)
+dlistint_t *new_node2(const int n)
 {
 	dlistint_t *new = NULL;
 
