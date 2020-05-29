e141aeaa37a9da4270108a8d803f750117a83aa9
Crashed Code <s.agudelo1200@protonmail.com>
2020-05-29 20:49:51 +0000

    betty fixed

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,29 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: hash table
+ * @key: key
+ * Return: the value associated with the element
+ * or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int idx;
-    hash_node_t *node = 0;
+	unsigned long int idx;
+	hash_node_t *node = 0;
 
-    if (!ht || !strlen(key))
-        return (0);
+	if (!ht || !strlen(key))
+		return (0);
 
-    idx = key_index((const unsigned char *)key, ht->size);
-    node = (ht->array)[idx];
+	idx = key_index((const unsigned char *)key, ht->size);
+	node = (ht->array)[idx];
 
-    if (!node)
-        return (0);
+	if (!node)
+		return (0);
 
-    while (node)
-    {
-        if (!strcmp(node->key, key))
-            return (node->value);
-        node = node->next;
-    }
-    return (0);
+	while (node)
+	{
+		if (!strcmp(node->key, key))
+			return (node->value);
+		node = node->next;
+	}
+	return (0);
 }
