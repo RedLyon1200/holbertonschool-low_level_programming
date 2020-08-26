e75d9103349c42535164fce6515f1358c56160d8
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-26 18:07:26 +0000

    for changed from = to <

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	for (i = 0; i = size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%i] = [%i]\n", i, array[i]);
 
